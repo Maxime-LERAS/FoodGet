fafc23eed04c14143027dedf103691da0fac6277
hasnaaelalaoui <hasnaa.elalaoui-m'hamdy@univ-cotedazur.fr>
2019-03-09 18:10:18 +0100

    Creer nouvelle liste contrôleur

---
+++ src/program/views/CreateListView.java
@@ -0,0 +1,9 @@
+package program.views;
+
+public class CreateListView {
+    public static String XML_FILE = "/program/resources/fxml/CreerListe-Popup.fxml";
+    public static String CSS_FILE = "/program/resources/styles/mainStyle.css";
+    public static final String LABEL = "Créer une nouvelle liste";
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 220;
+}
