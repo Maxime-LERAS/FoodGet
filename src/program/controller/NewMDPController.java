4f07511036edd866d7ac09e9014531251c62f886
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2019-03-10 13:20:16 +0100

    Possibilité de modifier son mot de passe.

---
+++ src/program/controller/NewMDPController.java
@@ -0,0 +1,63 @@
+package program.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import program.model.CommonPageCreator;
+import program.model.FoodGetUser;
+
+public class NewMDPController {
+
+
+    @FXML
+    private Button New_mdp_valider;
+
+    @FXML
+    private PasswordField New_mdp_new_mdp;
+
+    @FXML
+    private Hyperlink New_mdp_retour;
+
+    @FXML
+    private Label emptyFields;
+
+
+    private Stage stage;
+    private CommonPageCreator cr;
+    private FoodGetUser user;
+
+    public NewMDPController(Stage stage, FoodGetUser user) {
+        this.stage = stage;
+        this.user = user;
+        cr = new CommonPageCreator(this.stage, user);
+    }
+
+    public void init() {
+        emptyFields.setText("");
+        New_mdp_valider.setOnAction(event -> checkForm());
+        New_mdp_retour.setOnAction(event -> cr.openMonCompte());
+        this.stage.getScene().setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                checkForm();
+                keyEvent.consume();
+            }
+        });
+    }
+
+    public void checkForm() {
+        String mdp_Field = New_mdp_new_mdp.getCharacters().toString();
+        if (mdp_Field.equals("")) {
+            showEmptyFieldError();
+        }
+        else {
+            this.user.setPassword(mdp_Field);
+            cr.openMonCompte();
+        }
+    }
+
+    private void showEmptyFieldError() {
+        New_mdp_new_mdp.clear();
+        emptyFields.setText("Champ vide !");
+    }
+}
--- src/program/views/NewMDPView.java
+++ src/program/views/NewMDPView.java
@@ -1,4 +1,9 @@
 package program.views;
 
 public class NewMDPView {
+    public static String XML_FILE = "/program/resources/fxml/New_MDP.fxml";
+    public static String CSS_FILE = "/program/resources/styles/mainStyle.css";
+    public static final String LABEL = "FoodGet";
+    public static final int WIDTH = 720;
+    public static final int HEIGHT = 480;
 }
