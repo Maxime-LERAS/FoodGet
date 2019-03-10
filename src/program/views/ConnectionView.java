abd710be01a6382df7bb107748603b8e5ffbb84c
AlexMendezS <43187036+AlexMendezS@users.noreply.github.com>
2019-03-10 19:35:56 +0100

    correction de la view Connection

--- src/program/views/ConnectionView.java
+++ src/program/views/ConnectionView.java
@@ -2,7 +2,7 @@ package program.views;
 
 public class ConnectionView extends View {
 
-    public static String XML_FILE = "/program/resources/fxml/Connection.fxml";
+    public static String XML_FILE = "/program/resources/fxml/MyConnection.fxml";
     public static String CSS_FILE = "/program/resources/styles/mainStyle.css";
     public static final String LABEL = "FoodGet";
     public static final int WIDTH = 720;
